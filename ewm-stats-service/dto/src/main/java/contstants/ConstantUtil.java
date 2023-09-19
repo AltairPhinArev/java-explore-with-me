a9146618826ef7a0a28745eabb6959ee92cbaf6f
Andrey Panasiuk <andreypnick@gmail.com>
2023-09-19 18:12:09 +0300

    add-stats-service/fix-constants

--- ewm-stats-service/dto/src/main/java/contstants/ConstantUtil.java
+++ ewm-stats-service/dto/src/main/java/contstants/ConstantUtil.java
@@ -2,6 +2,6 @@ package contstants;
 
 public class ConstantUtil {
 
-    public final static String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    public final static String STAT_SERVICE_URL = "${stats-service.url}";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String STAT_SERVICE_URL = "${stats-service.url}";
 }
