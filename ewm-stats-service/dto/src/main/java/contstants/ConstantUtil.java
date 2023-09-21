e47fe51524d55a5914a5ae9325b034202f08727b
Andrey Panasiuk <andreypnick@gmail.com>
2023-09-21 13:24:29 +0300

    refactor: STATS_URL ConstantUtil

--- ewm-stats-service/dto/src/main/java/contstants/ConstantUtil.java
+++ ewm-stats-service/dto/src/main/java/contstants/ConstantUtil.java
@@ -3,5 +3,5 @@ package contstants;
 public class ConstantUtil {
 
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    public static final String STAT_SERVICE_URL = "${stats-service.url}";
+    public static final String STAT_SERVICE_URL = "http://localhost:9090";
 }
